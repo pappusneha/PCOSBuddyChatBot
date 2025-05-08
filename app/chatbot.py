0c10702c7075048fd40c0e97e858b554df9dcf30
pappusneha <pappusneha@gmail.com>
2025-05-08 16:33:06 -0400

    updates to chatbot with integration with openAI

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,13 +1,50 @@
-def get_response(message: str) -> str:
-    message = message.lower()
+import os
+from dotenv import load_dotenv
+from openai import OpenAI, OpenAIError
+from fastapi import FastAPI, HTTPException
+from models import ChatRequest, ChatResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-    if "symptom" in message or "track" in message:
-        return "You can log symptoms like acne, fatigue, cramps, and mood swings using the PCOSBuddy tracker."
-    elif "diet" in message:
-        return "A low-GI diet with balanced protein and fiber can help manage PCOS symptoms. Would you like some meal suggestions?"
-    elif "exercise" in message:
-        return "Moderate workouts like cycling, yoga, or walking for 30 mins a day are helpful for PCOS."
-    elif "hello" in message or "hi" in message:
-        return "Hi! I’m your PCOSBuddy Chatbot. How can I help you today?"
-    else:
-        return "I'm still learning! For detailed support, please talk to a health expert or check our symptom guide."
+load_dotenv()
+my_api_key = os.getenv("OPENAI_API_KEY")
+# print("api_key", my_api_key)
+
+client = OpenAI(api_key=my_api_key)
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.post("/")
+def chat_prompt(request: ChatRequest):
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant."
+                },
+                {
+                    "role": "user",
+                    "content": request.prompt
+                }
+            ]
+        )
+        gpt_response = completion.choices[0].message.content
+        return ChatResponse(response=gpt_response)
+
+    except OpenAIError as e:
+        raise HTTPException(status_code=500, detail=f"OpenAI api error: {str(e)}")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 
+
 class ChatRequest(BaseModel):
-    message: str
+    prompt: str
+
 
 class ChatResponse(BaseModel):
     response: str
